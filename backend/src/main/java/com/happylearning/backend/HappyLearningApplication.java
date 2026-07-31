e0c3c784bdedd4b399d70e13bf5a2569f1404ed7
rakesh <gollarakesh266@gmail.com>
2026-07-31 12:46:01 +0530

    started soringboot

---
+++ backend/src/main/java/com/happylearning/backend/HappyLearningApplication.java
@@ -0,0 +1,13 @@
+package com.happylearning.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyLearningApplication.class, args);
+    }
+
+}
